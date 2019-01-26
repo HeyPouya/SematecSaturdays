0cfd3d707d3a6315eabdf5a87b188d96ff7cc8ef
Pouya <pouyaheydary@gmail.com>
2019-01-26 18:51:42 +0330

    added lifecycle fo mainActivity

--- app/src/main/java/ir/sematec/saturdays/MainActivity.java
+++ app/src/main/java/ir/sematec/saturdays/MainActivity.java
@@ -3,6 +3,7 @@ package ir.sematec.saturdays;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        Log.d("MYAPPSTAG","onCreate Called");
 
-        //
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("MYAPPSTAG","onStart Called");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MYAPPSTAG","onResume Called");
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MYAPPSTAG","onPause Called");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MYAPPSTAG","onStop Called");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MYAPPSTAG","onDestroy Called");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
     }
 }
